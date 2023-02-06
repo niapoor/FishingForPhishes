fcaa188711a17c88efb05597b00305f9d320e502
niapoor <niapoor@gmail.com>
2023-02-06 10:03:53 -0500

    added some basic player states and animations

--- Game1.cs
+++ Game1.cs
@@ -22,6 +22,8 @@ namespace ImagineRITGame
         private GraphicsDeviceManager _graphics;
         private SpriteBatch _spriteBatch;
 
+        // Player objects
+        private Player player;
 
         // Keeps track of the game state so we know which
         // screen of the game to be on
@@ -39,13 +41,10 @@ namespace ImagineRITGame
         // Fields that hold the mouse's current and last state
         private MouseState mouseState;
         private MouseState previousMouseState;
-        // Gamepad stats
-        private GamePadState previousGPState;
-        private bool isController;
 
 
-        int _width = 0;
-        int _height = 0;
+        int _width;
+        int _height;
 
         // Space for sound effects
 
@@ -53,6 +52,7 @@ namespace ImagineRITGame
 
         // Player texture fields
         private Texture2D playerTexture;
+        private Texture2D fishingBobTexture;
 
         // Sprite sheet data
         private int numSpritesInSheet;
@@ -68,7 +68,6 @@ namespace ImagineRITGame
         public Game1()
         {
             _graphics = new GraphicsDeviceManager(this);
-            //_graphics.IsFullScreen = true;
             SetFullscreen();
             Content.RootDirectory = "Content";
             IsMouseVisible = true;
@@ -98,9 +97,12 @@ namespace ImagineRITGame
         {
             _spriteBatch = new SpriteBatch(GraphicsDevice);
 
-            playerTexture = Content.Load<Texture2D>("char_free_tmp");
+            playerTexture = Content.Load<Texture2D>("char_all");
+            fishingBobTexture = Content.Load<Texture2D>("inv_items");
             numSpritesInSheet = 8;
             widthOfSingleSprite = playerTexture.Width / numSpritesInSheet;
+
+            player = new Player(playerTexture, new Vector2(100, 0), fishingBobTexture);
 
             // Set up animation data, too
             fps = 7.0;
@@ -126,29 +128,12 @@ namespace ImagineRITGame
         /// <param name="gameTime">Info about time from MonoGame</param>
         private void UpdateAnimation(GameTime gameTime)
         {
-            // ElapsedGameTime is how the last GAME frame took
-            timeCounter += gameTime.ElapsedGameTime.TotalSeconds;
-
-            // Has enough time passed to flip to the next frame?
-            if (timeCounter >= secondsPerFrame)
-            {
-                // Change which frame is active,
-                // ensuring we go back to 1 eventually
-                playerCurrentFrame++;
-                if (playerCurrentFrame >= 8) // hardcoded here because I KNOW what my spritesheet looks like
-                {
-                    playerCurrentFrame = 1;
-                }
-
-                // Reset the time counter
-                timeCounter -= secondsPerFrame;
-            }
+            player.UpdateAnimation(gameTime);
         }
 
         protected override void Draw(GameTime gameTime)
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
-
             // Making it so that the drawn in assets don't become blurred when enlarged
             _spriteBatch.Begin
                 (SpriteSortMode.Immediate,
@@ -157,50 +142,7 @@ namespace ImagineRITGame
                 DepthStencilState.Default,
                 RasterizerState.CullCounterClockwise);
 
-            _spriteBatch.Draw(playerTexture,
-                new Rectangle(500, 345, widthOfSingleSprite * 9, (int)(playerTexture.Height / 1.5)),
-                new Rectangle((playerCurrentFrame) * widthOfSingleSprite, 0, widthOfSingleSprite, playerTexture.Height / 12),
-                Color.White,
-                0f,
-                Vector2.Zero,
-                0,
-                .826f);
-
-            _spriteBatch.Draw(playerTexture,
-                new Rectangle(500, 345, widthOfSingleSprite * 9, (int)(playerTexture.Height / 1.5)),
-                new Rectangle((playerCurrentFrame) * widthOfSingleSprite, (playerTexture.Height / 12) * 2, widthOfSingleSprite, playerTexture.Height / 12),
-                Color.White,
-                0f,
-                Vector2.Zero,
-                0,
-                .826f);
-
-            _spriteBatch.Draw(playerTexture,
-                new Rectangle(500, 345, widthOfSingleSprite * 9, (int)(playerTexture.Height / 1.5)),
-                new Rectangle((playerCurrentFrame) * widthOfSingleSprite, (playerTexture.Height / 12) * 4, widthOfSingleSprite, playerTexture.Height / 12),
-                Color.White,
-                0f,
-                Vector2.Zero,
-                0,
-                .826f);
-
-            _spriteBatch.Draw(playerTexture,
-                new Rectangle(500, 345, widthOfSingleSprite * 9, (int)(playerTexture.Height / 1.5)),
-                new Rectangle((playerCurrentFrame) * widthOfSingleSprite, (playerTexture.Height / 12) * 6, widthOfSingleSprite, playerTexture.Height / 12),
-                Color.White,
-                0f,
-                Vector2.Zero,
-                0,
-                .826f);
-
-            _spriteBatch.Draw(playerTexture,
-                new Rectangle(500, 345, widthOfSingleSprite * 9, (int)(playerTexture.Height / 1.5)),
-                new Rectangle((playerCurrentFrame) * widthOfSingleSprite, (playerTexture.Height / 12) * 8, widthOfSingleSprite, playerTexture.Height / 12),
-                Color.White,
-                0f,
-                Vector2.Zero,
-                0,
-                .826f);
+            player.Draw(_spriteBatch);
 
             _spriteBatch.End();
 
--- Player.cs
+++ Player.cs
@@ -6,6 +6,9 @@ using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Input;
 using Microsoft.Xna.Framework.Audio;
 using System.Dynamic;
+using SharpDX.Direct3D9;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement.Window;
+using SharpDX.Direct2D1;
 
 namespace ImagineRITGame
 {
@@ -14,13 +17,195 @@ namespace ImagineRITGame
     {
         Walking,
         Idle,
-        Fishing
+        Fishing,
+        HoldingFish
     }
 
     class Player : DynamicObject
     {
 
+        // The player's current state (walking, jumping, idle, or dying)
+        private PlayerStates playerState;
+        private PlayerStates prevState;
 
+        private Texture2D texture;
+        private Vector2 position;
+
+        // Keyboard states
+        private KeyboardState kbState;
+        private KeyboardState prevKBState;
+
+        // Fields for Player animation data
+        private double fps;
+        private double timeCounter;
+        private int playerCurrentFrame;
+        private int playerMaxFrame;
+        private double secondsPerFrame;
+        private int widthOfSingleSprite;
+        private int animationIndex;
+        private bool faceRight;
+        private int animationType;
+        private bool drawFishingBob;
+
+        /// <summary>
+        /// Getter and setter for the playerState
+        /// </summary>
+        public PlayerStates PlayerState
+        {
+            get { return playerState; }
+            set { playerState = value; }
+        }
+
+        /// <summary>
+        /// A set for the previous keyboard state
+        /// </summary>
+        public KeyboardState PrevKBState
+        {
+            set { prevKBState = value; }
+        }
+
+
+        public Player(Texture2D sprite, Vector2 location, Texture2D fishingBob)
+        {
+            texture = sprite;
+            position = location;
+
+            // The player is idle by default because the game starts with them not moving
+            playerState = PlayerStates.Idle;
+            prevState = PlayerStates.Idle;
+
+            faceRight = true;
+
+            // Initializing the keyboard states
+            prevKBState = new KeyboardState();
+            kbState = new KeyboardState();
+
+            // Assigning default values to animation data
+            fps = 10.0;
+            secondsPerFrame = 1 / fps;
+            timeCounter = 0;
+            playerCurrentFrame = 0;
+            playerMaxFrame = 4;
+            widthOfSingleSprite = 160;
+            animationIndex = 2;
+        }
+
+
+        /// <summary>
+        /// Helper for updating the player's animation based on time
+        /// </summary>
+        /// <param name="gameTime">Info about time from MonoGame</param>
+        public void UpdateAnimation(GameTime gameTime)
+        {
+            // Update the time since the last frame
+            timeCounter += gameTime.ElapsedGameTime.TotalSeconds;
+
+            // ================================================================
+            // For the different states below,
+            // vars change based on the current animation...
+            // playerMaxFrame: how many frames long the current animation is
+            // widthOfSingleSprite: some animations are "thinner" than others
+            // animationIndex: index of current animation on the spritesheet
+            // fps: some animations should run faster than others
+            // ================================================================
+
+            playerState = PlayerStates.Fishing;
+
+            if (playerState == PlayerStates.Fishing)
+            {
+                playerMaxFrame = 5;
+                widthOfSingleSprite = 160 / 5;
+                animationIndex = 0;
+                fps = 7.0;
+                animationType = 45;
+            }
+            else if (playerState == PlayerStates.Walking)
+            {
+                playerMaxFrame = 6;
+                widthOfSingleSprite = 160 / 5;
+                animationIndex = 1;
+                fps = 10.0;
+                animationType = 0;
+            }
+            else if (playerState == PlayerStates.Idle)
+            {
+                playerMaxFrame = 1; //1
+                widthOfSingleSprite = 160 / 5;
+                animationIndex = 2;
+                fps = 8.0;
+                animationType = 0; // 0
+            }
+
+            // Update secondsPerFrame, as fps could have been changed
+            secondsPerFrame = 1 / fps;
+
+            // Check that enough time has passed between frames
+            if (timeCounter >= secondsPerFrame)
+            {
+                // Increment the active frame
+                if((playerState == PlayerStates.Fishing && playerCurrentFrame != 4) || playerState != PlayerStates.Fishing)
+                {
+                    playerCurrentFrame++;
+                    drawFishingBob = true;
+                }
+                
+                if (playerCurrentFrame >= playerMaxFrame)
+                {
+                    playerCurrentFrame = 0;
+                    drawFishingBob = false;
+                }
+
+                // Reset the time counter for a new frame
+                timeCounter -= secondsPerFrame;
+            }
+        }
+
+
+        /// <summary>
+        /// Draws in the player facing the correct direction with the correct
+        /// frame of their current animation
+        /// </summary>
+        /// <param name="sb">the spritebatch that allows us to draw</param>
+        public void Draw(Microsoft.Xna.Framework.Graphics.SpriteBatch sb)
+        {
+            SpriteEffects spe = SpriteEffects.None;
+
+            if (!faceRight)
+                spe = SpriteEffects.FlipHorizontally;
+            
+            sb.Draw(texture,
+                new Rectangle(500, 100, widthOfSingleSprite * 9, (int)(texture.Height / 5)),
+                new Rectangle((playerCurrentFrame) * widthOfSingleSprite, (animationType * 32), widthOfSingleSprite, texture.Height / 44),
+                Color.White,
+                0f,
+                Vector2.Zero,
+                0,
+                .826f);
+
+            //if (drawFishingBob == true)
+            //{
+            //    sb.Draw(texture,
+            //        new Rectangle(500, 100, widthOfSingleSprite * 9, (int)(texture.Height / 5)),
+            //        new Rectangle((playerCurrentFrame) * widthOfSingleSprite, (animationType * 32), widthOfSingleSprite, texture.Height / 44),
+            //        Color.White,
+            //        0f,
+            //        Vector2.Zero,
+            //        0,
+            //        .826f);
+            //}
+
+        }
+
+
+        /// <summary>
+        /// Determines if the key being held was just released
+        /// </summary>
+        /// <param name="key">the key being held</param>
+        /// <returns>if the key being held was just released</returns>
+        private bool JustReleased(Keys key)
+        {
+            return Keyboard.GetState().IsKeyUp(key) && prevKBState.IsKeyDown(key);
+        }
 
     }
 
